62a023a814a9dbcd2e83a7c619d9998b474bb7e7
Allie McCarthy <mccarthy.allie@gmail.com>
2020-02-03 18:26:53 -0700

    change db name to follow pg name conventions

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/palette-picker',
+    connection: 'postgres://localhost/palette_picker',
     migrations: {
       directory: './db/migrations'
     },
