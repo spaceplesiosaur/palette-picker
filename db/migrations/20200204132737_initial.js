61612a838c8eb27923c7c9f5df2b5c05895ec07f
Allie McCarthy <mccarthy.allie@gmail.com>
2020-02-04 14:11:19 -0700

    add table migration columns

--- db/migrations/20200204132737_initial.js
+++ db/migrations/20200204132737_initial.js
@@ -1,8 +1,27 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('projects', function(table) {
+    table.increments('id').primary();
+    table.string('name');
+    table.boolean('current').defaultTo(false);
+  })
+  .createTable('palettes', function(table) {
+    table.increments('id').primary();
+    table.string('name');
+    table.string('color1');
+    table.string('color2');
+    table.string('color3');
+    table.string('color4');
+    table.string('color5');
+    table.integer('project_id');
+    table.foreign('project_id').references('projects.id')
+
+    table.timestamps(true, true);
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTable('palettes');
+  .dropTable('projects');
 };
