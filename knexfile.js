091ca277eb3bac6a4392b7e1acb7a7ea3382cd5f
Allie McCarthy <mccarthy.allie@gmail.com>
2020-02-04 13:28:24 -0700

    Add migration file and test environment

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,6 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
-
     production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
@@ -24,6 +23,16 @@ module.exports = {
       directory: "./db/seeds/dev"
     },
     useNullAsDefault: true
-  }
-
+  },
+    test: {
+      client: 'pg',
+      connection: 'postgres://localhost/palette_picker_test',
+      migrations: {
+        directory: './db/migrations'
+      },
+      seeds: {
+        directory: "./db/seeds/test"
+      },
+      useNullAsDefault: true,
+    },
 };
