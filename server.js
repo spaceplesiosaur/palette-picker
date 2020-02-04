12e0d3ae7530bf692c731bdb6fc9bef33a2aef1b
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-03 19:20:10 -0700

    Add variables needed for knex to talk with DB

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.use(cors());
 app.set('port', process.env.PORT || 3000);
