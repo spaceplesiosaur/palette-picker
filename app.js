8a3344acd97a2339677d88632016cda640da3da0
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-04 19:37:42 -0700

    Add patch and delete end point methods

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/api/v1/projects', (request, response) => {
   response.send('Pallete Picker is running');
 });
 
-app.get('/api/v1/palettes/:id', (request, response) => {
+app.get('/api/v1/palettes/:id', async (request, response) => {
   try {
     const pallete = await database('palletes').where('id', request.params.id);
     pallete.length ? response.status(200).send(pallete) : response.status(404).send({ error:'Pallete not found'});
@@ -36,7 +36,7 @@ app.get('/api/v1/projects:id', (request, response) => {
   response.send('Pallete Picker is running');
 });
 
-app.post('/api/v1/palettes', (request, response) => {
+app.post('/api/v1/palettes', async (request, response) => {
   const pallete = request.body;
   for(let palleteInfo of ['name', 'color1', 'color2', 'color3', 'color4', 'color5']) {
     !pallete.hasOwnProperty(palleteInfo) ? response.status(422).send({ error: `Expected format: { name: <String>, color1: <String>, color2: <String>, color3: <String>, color4: <String>, color5: <String>}. You're missing a "${palleteInfo}" property.` }) : '';
@@ -54,16 +54,26 @@ app.post('/api/v1/projects', (request, response) => {
   response.send('Pallete Picker is running');
 });
 
-app.patch('/api/v1/palettes/:id', (request, response) => {
-  response.send('Pallete Picker is running');
+app.patch('/api/v1/palettes/:id', async (request, response) => {
+  const { id } = request.params;
+  const { name } = request.body;
+  const pallete = await database('palletes').find((pallete) => pallete.id == id);
+
+  pallete.name = name;
+ !pallete.name ? response.sendStatus(404) : response.status(200).json(pallete);
 });
 
 app.patch('/api/v1/projects/:id', (request, response) => {
   response.send('Pallete Picker is running');
 });
 
-app.delete('/api/v1/palettes/:id', (request, response) => {
-  response.send('Pallete Picker is running');
+app.delete('/api/v1/palettes/:id', async (request, response) => {
+  try {
+    const pallete = await database('palletes').where('id', request.params.id).del();
+    pallete.length ? response.status(404).json({error: `Could not find pallete with id ${request.params.id}`}) : response.status(200).json(pallete);
+    } catch(error) {
+    response.status(500).json({ error });
+}
 });
 
 app.delete('/api/v1/projects/:id', (request, response) => {
