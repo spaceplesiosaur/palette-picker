021e69bdf82395b6108d6b22585643c0a50941f3
Allie McCarthy <mccarthy.allie@gmail.com>
2020-02-04 19:23:42 -0700

    Add GET requests

--- app.js
+++ app.js
@@ -14,16 +14,33 @@ app.get('/api/v1/palettes', (request, response) => {
   response.send('Pallete Picker is running');
 });
 
-app.get('/api/v1/projects', (request, response) => {
-  response.send('Pallete Picker is running');
+app.get('/api/v1/projects', async (request, response) => {
+  try {
+    const projectList = await database('projects').select()
+    if(!projectList.length) {
+      response.status(404).json({error: 'Unable to find that list'})
+    }
+    response.status(200).json(projectList)
+  } catch(error) {
+    response.status(500).json({ error })
+  }
 });
 
 app.get('/api/v1/palettes/:id', (request, response) => {
   response.send('Pallete Picker is running');
 });
 
-app.get('/api/v1/projects:id', (request, response) => {
-  response.send('Pallete Picker is running');
+app.get('/api/v1/projects/:id', async (request, response) => {
+  try {
+    const { id } = request.params
+    const chosenProject = await database('projects').where('id', id)
+    if(!projectList.length) {
+      response.status(422).json({error: 'Unable to find that project'})
+    }
+    response.status(200).json(chosenProject)
+  } catch(error) {
+    response.status(500).json({ error })
+  }
 });
 
 app.post('/api/v1/palettes', (request, response) => {
@@ -55,5 +72,7 @@ app.get('/', (request, response) => {
 });
 
 app.get('*', (request, response) => {
-  response.status(404).send('Pallete Not Found');
+  response.status(404).send('This page does not exist');
 });
+
+export default app;
