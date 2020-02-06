17aeabd8ed8c1a35961fd73295b905245febe915
Allie McCarthy <mccarthy.allie@gmail.com>
2020-02-06 13:20:06 -0700

    Add GET test

--- app.test.js
+++ app.test.js
@@ -10,27 +10,45 @@ describe('Server', () => {
   beforeEach(async () => {
     await database.seed.run()
   })
- 
+
   describe('init', () => {
     it('should return a 200 status', async () => {
       const res = await request(app).get('/')
       expect(res.status).toBe(200)
     });
   });
-});
 
-describe('GET /api/v1/palettes', () => {
-    it('should return a 200 and all of the palletes', async () => {
-      const expectedPalettes = await database('palettes').select();
 
-      const response = await request(app).get('/api/v1/palettes');
-      const palletes = response.body;
-  
-      expect(response.status).toBe(200);
-      expect(palletes).toEqual(expectedPalettes);
-      
+  describe('GET /api/v1/palettes', () => {
+      it('should return a 200 and all of the palletes', async () => {
+        const expectedPalettes = await database('palettes').select();
+        const cleanedPalettes = JSON.parse(JSON.stringify(expectedPalettes))
+        const response = await request(app).get('/api/v1/palettes');
+        const palletes = response.body;
+
+        expect(response.status).toBe(200);
+        expect(palletes).toEqual(cleanedPalettes);
+      })
+
+  });
+
+  describe('GET /api/v1/projects', () => {
+      it('should return a 200 and all of the projects', async () => {
+        const expectedProjects = await database('projects').select();
+
+        const response = await request(app).get('/api/v1/projects');
+        const projects = response.body;
+
+        expect(response.status).toBe(200);
+        expect(projects).toEqual(expectedProjects);
+
+      })
+  });
+  describe('GET api/v1/projects/:id', () => {
+    it('should return a 200 status and a single project if the project exists', async () => {
+
     })
+  })
+
+
 });
-
-
-
